6e7d588ea51f89c9dd00992bdcffecc6771ece99
Nikolai Mavrenkov <koluch@koluch.ru>
2021-02-22 00:17:11 +0300

    Use production stripe key

--- services/env.ts
+++ services/env.ts
@@ -2,4 +2,4 @@
 export const MAIN_SITE_URL = "https://he-he.org/";
 export const PRISMIC_URL = "https://he-he-birthday-2021.cdn.prismic.io/api/v2";
 export const STRIPE_PUBLISHABLE_KEY =
-  "pk_test_gpUMCtiEpoRyqVMMi3iEvE1D00JwfBZ9EF";
+  "pk_live_0rEgoe4LSNQL22eihCHkXTr70025zna97b";
