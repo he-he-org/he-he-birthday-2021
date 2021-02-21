84187339061815edf658ec31f03d2a61ba77d0d6
Nikolai Mavrenkov <koluch@koluch.ru>
2021-02-21 20:11:51 +0300

    Change public key

--- services/env.ts
+++ services/env.ts
@@ -2,4 +2,4 @@
 export const MAIN_SITE_URL = "https://he-he.org/";
 export const PRISMIC_URL = "https://he-he-birthday-2021.cdn.prismic.io/api/v2";
 export const STRIPE_PUBLISHABLE_KEY =
-  "pk_test_51H220oG6m2c66h5u7idfd13mxUxUsrWgTQQxNChDn7my3QHUfTUJvCoZqKSBUR4twsxrcyxkTHpIIPlY8SWkOO6800gbgSIxVm";
+  "pk_test_gpUMCtiEpoRyqVMMi3iEvE1D00JwfBZ9EF";
