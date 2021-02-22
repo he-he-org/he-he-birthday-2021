a2b47796c1bc0054f51b74d644b82d0484214ebf
Nikolai Mavrenkov <koluch@koluch.ru>
2021-02-22 23:50:52 +0300

    Fix types

--- components/prismic/Image/Image.tsx
+++ components/prismic/Image/Image.tsx
@@ -1,10 +1,13 @@
 import { PrismicImage } from "../../../api";
 
 interface Props {
+  className?: string | undefined;
   image: PrismicImage;
 }
 
 export default function Image(props: Props): JSX.Element {
-  const { image } = props;
-  return <img src={image.url} style={{ maxWidth: "100%" }} />;
+  const { className, image } = props;
+  return (
+    <img className={className} src={image.url} style={{ maxWidth: "100%" }} />
+  );
 }
